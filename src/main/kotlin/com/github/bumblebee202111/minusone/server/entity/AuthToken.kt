d6ff70735653523ba87bf1e4994d391c825e6000
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2026-04-21 23:17:20 +0800

    fix(auth): set length and ASCII charset for AuthToken primary key to prevent Data Truncation error
    GitOrigin-RevId: f125952a94a21133eb240831b3984f808738a760

--- src/main/kotlin/com/github/bumblebee202111/minusone/server/entity/AuthToken.kt
+++ src/main/kotlin/com/github/bumblebee202111/minusone/server/entity/AuthToken.kt
@@ -7,6 +7,10 @@ import java.time.Instant
 @Table(name = "auth_tokens")
 data class AuthToken(
     @Id
+    @Column(
+        length = 1024,
+        columnDefinition = "VARCHAR(1024) CHARACTER SET ascii COLLATE ascii_general_ci"
+    )
     val token: String,
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "account_id", nullable = false)
