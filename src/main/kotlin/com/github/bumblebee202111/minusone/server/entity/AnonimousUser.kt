c7ed630c5e41173409c7f405ec6f3a536f435e20
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2026-04-21 23:57:32 +0800

    fix(entity): Remove automatic ID generation for AnonimousUser
    GitOrigin-RevId: 7884d1a9e6f0a89b8ef33c52ccd829afcf269125

--- src/main/kotlin/com/github/bumblebee202111/minusone/server/entity/AnonimousUser.kt
+++ src/main/kotlin/com/github/bumblebee202111/minusone/server/entity/AnonimousUser.kt
@@ -1,8 +1,6 @@
 package com.github.bumblebee202111.minusone.server.entity
 
 import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 
@@ -10,7 +8,6 @@ import jakarta.persistence.Table
 @Table(name = "anonimous_users")
 data class AnonimousUser(
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val userId: Long = 0L,
+    val userId: Long,
     val createTime: Long
 )
